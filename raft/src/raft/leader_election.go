2c0c45d3f81ec2510092f1f8f04002533b08fbd9
johnlijianian <1033096695@qq.com>
2022-04-07 22:05:57 +0800

    raft-first

---
+++ raft/src/raft/leader_election.go
@@ -0,0 +1,38 @@
+package raft
+
+import (
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func (rf *Raft) resetElectionTimer() {
+	t := time.Now()
+	electionTimeout := time.Duration(150 + rand.Intn(150)) * time.Millisecond
+	rf.electionTime = t.Add(electionTimeout)
+}
+
+func (rf *Raft) leaderElection() {
+	rf.currentTerm++
+	rf.state = Candidate
+	rf.votedFor = rf.me // 给自己投一票
+	// rf.persist()
+	rf.resetElectionTimer()
+	term := rf.currentTerm
+	voteCounter := 1
+	lastLog := rf.log.lastLog()
+	DPrintf("[%v]: start leader election, term %d\n", rf.me, rf.currentTerm)
+	args := RequestVoteArgs {
+		Term: term,
+		CandidateId: rf.me,
+		LastLogIndex: lastLog.Index,
+		LastLogTerm: lastLog.Term,
+	}
+
+	var becomeLeader sync.once
+	for serverId, _ := range rf.peers {
+		if serverId != rf.me {
+			go rf.CandidateRequestVote(serverId, &args, &voteCounter, &becomeLeader)
+		}
+	}
+}
